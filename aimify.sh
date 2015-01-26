3454217b8fe069b50ebace54d407df07fe1a89ef
Julia Hansbrough <julia@flowerhack.com>
2015-01-26 01:13:33 -0600

    Small tweaks

--- aimify.sh
+++ aimify.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 # Add silly decorative characters around the first argument, a la AIM
 
-input=$1
+output=$1
 
 bling_chars=(xXx XxX '~' ^ • ¥ + '||')
+len_bling_chars=${#bling_chars[@]}
 
 random_bling=""
 random_length=$((3 + RANDOM%3))
 
-for i in `seq $random_length`
+echo $random_length
+
+for i in eval echo {0..$random_length}
 do
-  random_bling="$random_bling${bling_chars[$((RANDOM%8))]}"
+  echo i
+  char_pos=$((RANDOM % len_bling_chars))
+  output="${bling_chars[$char_pos]}$output${bling_chars[$char_pos]}"
 done
 
-rev_random_bling=$(echo $random_bling | rev)
-
-echo $random_bling $input $rev_random_bling
+echo $output
