b1689b74a83782b4ff72d58db0fcd9f9d15ead4f
Julia Hansbrough <julia@flowerhack.com>
2015-01-26 01:19:59 -0600

    fixed echo eval bug

--- aimify.sh
+++ aimify.sh
@@ -9,11 +9,8 @@ len_bling_chars=${#bling_chars[@]}
 random_bling=""
 random_length=$((3 + RANDOM%3))
 
-echo $random_length
-
-for i in eval echo {0..$random_length}
+for i in $(eval echo {0..$random_length})
 do
-  echo i
   char_pos=$((RANDOM % len_bling_chars))
   output="${bling_chars[$char_pos]}$output${bling_chars[$char_pos]}"
 done
